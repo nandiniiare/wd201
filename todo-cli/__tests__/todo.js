99d66ae61f1f69db38b6441405ea0f11a647ac3a
Nandini <donetisaipavansurya@gmail.com>
2023-12-13 03:10:25 +0530

    Add new feature

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,25 +1,11 @@
 // __tests__/todo.js
 const SequelizeMock = require("sequelize-mock");
-
-
 const dbMock = new SequelizeMock();
-// Lazy function to create and return the TodoMock
-const createTodoMock = () => {
-  return dbMock.define("Todo", {
-    title: "Test Todo",
-    dueDate: todo.getJSDate(5),
-    completed: false,
-  });
-};
-const todo = require("../todo"); // Import todo module first
-jest.mock("../models", () => ({
-  Todo: createTodoMock(),
-}));
+const todo = require("../todo");
 
 describe("Tests for functions in todo.js", function () {
-  beforeEach(() => {
-    // Clear the mock database before each test
-    dbMock.clearAll();
+  beforeAll(async () => {
+    await dbMock.sync({ force: true });
   });
 
   test("Test to add a new todo", async () => {
@@ -43,21 +29,16 @@ describe("Tests for functions in todo.js", function () {
 
     await todo.markAsComplete(todoToMark.id);
 
-    const updatedTodo = await todo.getTask(todoToMark.id);
+    const updatedTodo = await dbMock.Todo.findByPk(todoToMark.id);
     expect(updatedTodo.completed).toBe(true);
   });
 
   test("Test to retrieve overdue items", async () => {
-    const overdueTodoData = {
+    const overdueTodo = await todo.addTask({
       title: "Overdue Todo",
       dueDate: todo.getJSDate(-2),
       completed: false,
-    };
-
-    // Mock the create method
-    TodoMock.create.mockResolvedValue(overdueTodoData);
-
-    const overdueTodo = await todo.addTask(overdueTodoData);
+    });
 
     const overdueItems = await todo.overdue();
     expect(overdueItems.length).toBe(1);
@@ -65,16 +46,11 @@ describe("Tests for functions in todo.js", function () {
   });
 
   test("Test to retrieve due today items", async () => {
-    const dueTodayTodoData = {
+    const dueTodayTodo = await todo.addTask({
       title: "Due Today Todo",
       dueDate: todo.getJSDate(0),
       completed: false,
-    };
-
-    // Mock the create method
-    TodoMock.create.mockResolvedValue(dueTodayTodoData);
-
-    const dueTodayTodo = await todo.addTask(dueTodayTodoData);
+    });
 
     const dueTodayItems = await todo.dueToday();
     expect(dueTodayItems.length).toBe(1);
@@ -82,36 +58,53 @@ describe("Tests for functions in todo.js", function () {
   });
 
   test("Test to retrieve due later items", async () => {
-    const dueLaterTodoData = {
+    const dueLaterTodo = await todo.addTask({
       title: "Due Later Todo",
       dueDate: todo.getJSDate(2),
       completed: false,
-    };
-
-    // Mock the create method
-    TodoMock.create.mockResolvedValue(dueLaterTodoData);
-
-    const dueLaterTodo = await todo.addTask(dueLaterTodoData);
+    });
 
     const dueLaterItems = await todo.dueLater();
     expect(dueLaterItems.length).toBe(1);
     expect(dueLaterItems[0].id).toBe(dueLaterTodo.id);
   });
 
-  test("Test toDisplayableList function", async () => {
-    // Mock some todos in the database
-    const todosData = [
-      { id: 1, title: "Todo 1", dueDate: todo.getJSDate(1), completed: false },
-      { id: 2, title: "Todo 2", dueDate: todo.getJSDate(2), completed: true },
-      // Add more as needed
-    ];
+  // Additional Test Cases
 
-    // Mock the findAll method
-    TodoMock.findAll.mockResolvedValue(todosData);
+  test("Test to mark a completed todo as incomplete", async () => {
+    const completedTodo = await todo.addTask({
+      title: "Completed Todo",
+      dueDate: todo.getJSDate(1),
+      completed: true,
+    });
 
-    const displayableList = await todo.toDisplayableList();
+    await todo.markAsIncomplete(completedTodo.id);
 
-    expect(displayableList.length).toBe(todosData.length);
-    // Add more assertions based on the expected format of the displayable list
+    const updatedTodo = await dbMock.Todo.findByPk(completedTodo.id);
+    expect(updatedTodo.completed).toBe(false);
+  });
+
+  test("Test to retrieve completed items", async () => {
+    const completedTodo = await todo.addTask({
+      title: "Completed Todo",
+      dueDate: todo.getJSDate(1),
+      completed: true,
+    });
+
+    const completedItems = await todo.completed();
+    expect(completedItems.length).toBe(1);
+    expect(completedItems[0].id).toBe(completedTodo.id);
+  });
+
+  test("Test to retrieve items that are not completed", async () => {
+    const incompleteTodo = await todo.addTask({
+      title: "Incomplete Todo",
+      dueDate: todo.getJSDate(3),
+      completed: false,
+    });
+
+    const incompleteItems = await todo.incomplete();
+    expect(incompleteItems.length).toBe(1);
+    expect(incompleteItems[0].id).toBe(incompleteTodo.id);
   });
 });
