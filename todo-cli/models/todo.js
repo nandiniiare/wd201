5d60a86bf2cbebc62b070d56b15c62751076bbd3
Nandini <donetisaipavansurya@gmail.com>
2023-12-16 00:42:44 +0530

    finished

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -12,11 +12,46 @@ module.exports = (sequelize, DataTypes) => {
     static async addTask(params) {
       return  await Todo.create(params);
     }
-    static associate(models) {
-      // define association here
-    }displayableString() {
-      let checkbox = this.completed ? "[x]" : "[ ]";
-      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+    static async overdue() {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.lt]: new Date() },
+        },
+      });
+    }
+    static async dueToday() {
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.between]: [new Date(), new Date(new Date().setHours(23, 59, 59, 999))],
+          },
+        },
+      });
+    }
+    static async dueLater() {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.gt]: new Date() },
+        },
+      });
+    }
+    static async markAsComplete(id) {
+      const todo = await Todo.findByPk(id);
+      if (todo) {
+        todo.completed = true;
+        await todo.save();
+      }
+    }                
+    displayableString() {
+      let checkbox = this.completed ? '[x]' : '[ ]';
+
+      let dateToday = new Date().toISOString().split('T')[0];
+
+      if (this.dueDate === dateToday) {
+        return `${this.id}. ${checkbox} ${this.title}`;
+      } else {
+        return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+      }
     }
   }
   Todo.init({
