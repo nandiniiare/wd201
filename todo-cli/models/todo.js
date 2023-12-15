47214a26111de73116a953e3966cf0fabf7ce95b
Nandini <donetisaipavansurya@gmail.com>
2023-12-16 00:46:30 +0530

    finished

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -10,48 +10,74 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */ 
     static async addTask(params) {
-      return  await Todo.create(params);
+      return await Todo.create(params);
     }
+    static async showList() {
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+      const overdueItems = await Todo.overdue();
+      overdueItems.forEach((item) => console.log(item.displayableString()));
+      console.log();
+
+      console.log("Due Today");
+      const dueTodayItems = await Todo.dueToday();
+      dueTodayItems.forEach((item) => console.log(item.displayableString()));
+      console.log();
+
+      console.log("Due Later");
+      const dueLaterItems = await Todo.dueLater();
+      dueLaterItems.forEach((item) => console.log(item.displayableString()));
+    }
+
     static async overdue() {
       return await Todo.findAll({
         where: {
-          dueDate: { [Op.lt]: new Date() },
-        },
-      });
-    }
-    static async dueToday() {
-      return await Todo.findAll({
-        where: {
           dueDate: {
-            [Op.between]: [new Date(), new Date(new Date().setHours(23, 59, 59, 999))],
+            [Op.lt]: new Date(),
           },
         },
       });
     }
-    static async dueLater() {
+
+    static async dueToday() {
       return await Todo.findAll({
         where: {
-          dueDate: { [Op.gt]: new Date() },
+          dueDate: {
+            [Op.eq]: new Date(),
+          },
         },
       });
     }
+
+    static async dueLater() {
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
+        },
+      });
+    }
+
     static async markAsComplete(id) {
       const todo = await Todo.findByPk(id);
       if (todo) {
         todo.completed = true;
+        console.log(`Todo with ID ${id} marked as complete.`);
         await todo.save();
-      }
-    }                
-    displayableString() {
-      let checkbox = this.completed ? '[x]' : '[ ]';
-
-      let dateToday = new Date().toISOString().split('T')[0];
-
-      if (this.dueDate === dateToday) {
-        return `${this.id}. ${checkbox} ${this.title}`;
       } else {
-        return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+        throw new Error(`Todo with id ${id} not found`);
       }
+    }
+
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+
+      if (this.dueDate === new Date().toLocaleDateString("en-CA")) {
+        return `${this.id}. ${checkbox} ${this.title}`;
+      }
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
     }
   }
   Todo.init({
