c8ebaccb10ac4b67a63df42452de71f889042a62
Nandini <donetisaipavansurya@gmail.com>
2024-01-05 11:24:38 +0530

    final

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const path = require("path");
 app.use(bodyParser.json());
-const { Todo } = require("./models");
 
+const { Todo } = require("./models");
+app.set("view engine","ejs");
+
+app.get("/", async (request,response) => {
+   const allTodos = await Todo.getTodos();
+   if( request.accepts('html')){
+      response.render('index', {
+         allTodos
+      });
+   }else{
+      response.json({
+         allTodos
+      })
+   }
+});
+app.use(express.static(path.join(__dirname,'public')));
 app.get("/todos", async (request, response) => {
    try {
       const todos = await Todo.findAll();
