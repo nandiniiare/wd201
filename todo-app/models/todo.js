6659c459d04856163f6d6d34b5547e9b23c85b9f
Nandini <donetisaipavansurya@gmail.com>
2024-01-06 00:42:11 +0530

    three

---
+++ todo-app/models/todo.js
@@ -0,0 +1,48 @@
+'use strict';
+const {Model} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) { 
+      // define association here
+    }
+    static addTodo({title, dueDate}){
+      return this.create({ title: title, dueDate: dueDate, completed: false})
+    }
+    markAsCompleted() {
+      return this.update({ completed: true });
+    } 
+    static deleteTodo(id) {
+      return this.destroy({ where: { id } });
+    }       
+  }
+  Todo.init({
+    title: {
+       type: DataTypes.STRING,
+       allowNull: false,
+       validate: {
+          notEmpty: true,
+       },
+    },
+    dueDate: {
+       type: DataTypes.DATEONLY,
+       allowNull: false,
+       validate: {
+          isDate: true,
+       },
+    },
+    completed: {
+       type: DataTypes.BOOLEAN,
+       defaultValue: false,
+    },
+ }, {
+    sequelize,
+    modelName: 'Todo',
+ });
+ 
+  return Todo;  
+};
